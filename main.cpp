4df39fbce08ed5e68ef2a734ce4957d4efe4977e
kmaune@umich.edu <kmaune@umich.edu>
2025-03-01 14:06:54 -0500

    save state, compiling

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "order.hpp"
 #include "order_pool.hpp"
 #include "price_level.hpp"
+#include "order_book.hpp"
 
 int main() {
   hft::Order order;
+  hft::OrderPool order_pool;
+  hft::PriceLevel price_level(100);
+  hft::OrderBook aapl_book("AAPL", order_pool);
 
   return 0;
 }
