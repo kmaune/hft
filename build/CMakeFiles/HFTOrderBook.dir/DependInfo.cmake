b094b3b0ab532bfe30eca74d0c51042f23bc03e5
kmaune@umich.edu <kmaune@umich.edu>
2025-03-01 19:52:24 -0500

    cmake build setup

---
+++ build/CMakeFiles/HFTOrderBook.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/kmaune/Desktop/coding/interview/hft/main.cpp" "CMakeFiles/HFTOrderBook.dir/main.cpp.o" "gcc" "CMakeFiles/HFTOrderBook.dir/main.cpp.o.d"
+  "/Users/kmaune/Desktop/coding/interview/hft/order_book.cpp" "CMakeFiles/HFTOrderBook.dir/order_book.cpp.o" "gcc" "CMakeFiles/HFTOrderBook.dir/order_book.cpp.o.d"
+  "/Users/kmaune/Desktop/coding/interview/hft/order_book_manager.cpp" "CMakeFiles/HFTOrderBook.dir/order_book_manager.cpp.o" "gcc" "CMakeFiles/HFTOrderBook.dir/order_book_manager.cpp.o.d"
+  "/Users/kmaune/Desktop/coding/interview/hft/order_pool.cpp" "CMakeFiles/HFTOrderBook.dir/order_pool.cpp.o" "gcc" "CMakeFiles/HFTOrderBook.dir/order_pool.cpp.o.d"
+  "/Users/kmaune/Desktop/coding/interview/hft/price_level.cpp" "CMakeFiles/HFTOrderBook.dir/price_level.cpp.o" "gcc" "CMakeFiles/HFTOrderBook.dir/price_level.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
